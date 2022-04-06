47e816eae2d792c7645dc028ffeaac3d3553ebf8
Carla Uyemura <carla.uyemura@gmail.com>
2022-04-06 18:18:35 -0300

    Alteração de arquivo js

--- fundamentos/bloco-04-Introducao-a-javascript/Dia-1/script.js
+++ fundamentos/bloco-04-Introducao-a-javascript/Dia-1/script.js
@@ -6,3 +6,36 @@ birthYear = 2030;
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+/////////////////////////////////////////
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientAge);
+?
+//////////////////////////////////////////////
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2*(base + height);
+console.log(perimeter);
+
+/////////////////////////////////////////////
+
+const nota = 60;
+
+if(nota >= 80){
+  console.log("Parabens, voce foi aprovado!")
+
+}else if(nota < 80 && nota >= 60){
+  console.log()
+}
