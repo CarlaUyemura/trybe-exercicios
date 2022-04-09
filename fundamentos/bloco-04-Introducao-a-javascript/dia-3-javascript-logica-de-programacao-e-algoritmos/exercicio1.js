b1c0c5a5a3c5ff789f16a6cd999d510b19df71c9
Carla Uyemura <carla.uyemura@gmail.com>
2022-04-09 12:38:51 -0300

    Incluindo exercicio 3

--- fundamentos/bloco-04-Introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/bloco-04-Introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -1,10 +1,11 @@
+//1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero.
 
 let fatorial = 10;
 let resultado = fatorial;
 
 for(let i = 1; i < fatorial; i += 1){
 
-resultado *=i;
+resultado *= i;
 
 } console.log(resultado);
 
